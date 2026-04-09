9ca63c6834475abb197451f2911be8208dc9de81
GitHub Action <action@github.com>
2026-04-09 10:34:21 +0000

    Update dist js

--- pkg/cloudflare/worker/dist/main.js
+++ pkg/cloudflare/worker/dist/main.js
@@ -7058,80 +7058,89 @@ const ipaddr = __webpack_require__(640);
 
 
 
-
 const getZoneFromReqURL = (reqURL, actionsByDomain) => {
   // loop through
   for (const [domain] of Object.entries(actionsByDomain)) {
     // if the request URL contains the domain, return the actions
     if (reqURL.includes(domain)) {
-      return domain
+      return domain;
     }
   }
-}
+};
 
 const getSupportedActionForZone = (action, actionsForDomain) => {
   if (actionsForDomain["supported_actions"].includes(action)) {
-    return action
+    return action;
   }
-  return actionsForDomain["default_action"]
-}
+  return actionsForDomain["default_action"];
+};
 
-const handleTurnstilePost = async (request, body, turnstile_secret, zoneForThisRequest) => {
-  const token = body.get('cf-turnstile-response');
-  const ip = request.headers.get('CF-Connecting-IP');
+const handleTurnstilePost = async (
+  request,
+  body,
+  turnstile_secret,
+  zoneForThisRequest,
+) => {
+  const token = body.get("cf-turnstile-response");
+  const ip = request.headers.get("CF-Connecting-IP");
 
   let formData = new FormData();
 
-  formData.append('secret', turnstile_secret);
-  formData.append('response', token);
-  formData.append('remoteip', ip);
+  formData.append("secret", turnstile_secret);
+  formData.append("response", token);
+  formData.append("remoteip", ip);
 
-  const url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
+  const url = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
   const result = await fetch(url, {
     body: formData,
-    method: 'POST',
+    method: "POST",
   });
 
   const outcome = await result.json();
 
   if (!outcome.success) {
-    console.log('Invalid captcha solution');
-    return new Response('Invalid captcha solution', {
-      status: 401
+    console.log("Invalid captcha solution");
+    return new Response("Invalid captcha solution", {
+      status: 401,
     });
   } else {
-    console.log('Valid captcha solution;', "Issuing JWT token");
-    const jwtToken = await index_default.sign({
-      data: "captcha solved",
-      exp: Math.floor(Date.now() / 1000) + (2 * (60 * 60))
-    }, turnstile_secret + ip);
+    console.log("Valid captcha solution;", "Issuing JWT token");
+    const jwtToken = await index_default.sign(
+      {
+        data: "captcha solved",
+        exp: Math.floor(Date.now() / 1000) + 2 * (60 * 60),
+      },
+      turnstile_secret + ip,
+    );
     const newResponse = new Response(null, {
-      status: 302
-    })
-    newResponse.headers.set("Set-Cookie", `${zoneForThisRequest}_captcha=${jwtToken}; Path=/; HttpOnly; Secure; SameSite=Strict;`)
-    newResponse.headers.set("Location", request.url)
-    return newResponse
-
+      status: 302,
+    });
+    newResponse.headers.set(
+      "Set-Cookie",
+      `${zoneForThisRequest}_captcha=${jwtToken}; Path=/; HttpOnly; Secure; SameSite=Strict;`,
+    );
+    newResponse.headers.set("Location", request.url);
+    return newResponse;
   }
-}
+};
 
 const getFromKV = async (kv, key) => {
   try {
     const value = await kv.get(key);
     return value;
   } catch (e) {
-    console.log(e)
-    return null
+    console.log(e);
+    return null;
   }
-}
+};
 
 const writeToKV = async (kv, key, value) => {
   try {
     await kv.put(key, value);
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-}
+};
 
 // request ->
 // <-captcha
@@ -7140,55 +7149,69 @@ const writeToKV = async (kv, key, value) => {
 
 /* harmony default export */ const worker = ({
   async fetch(request, env, ctx) {
-
     const doBan = async () => {
-      return new Response(await getFromKV(env.CROWDSECCFBOUNCERNS, "BAN_TEMPLATE"), {
-        status: 403,
-        headers: { "Content-Type": "text/html" }
-      });
-    }
+      return new Response(
+        await getFromKV(env.CROWDSECCFBOUNCERNS, "BAN_TEMPLATE"),
+        {
+          status: 403,
+          headers: { "Content-Type": "text/html" },
+        },
+      );
+    };
 
     const doCaptcha = async (env, zoneForThisRequest) => {
       // Check if the request has proof of solving captcha
       // If the request has proof of solving captcha, let it pass through
       // If the request does not have proof of solving captcha. Check if the request is submission of captcha.
-      // If it's captcha submission, do the validation  and issue a JWT token as a cookie. 
+      // If it's captcha submission, do the validation  and issue a JWT token as a cookie.
       // Else return the captcha HTML
-      const ip = request.headers.get('CF-Connecting-IP');
-      let turnstileCfg = await getFromKV(env.CROWDSECCFBOUNCERNS, "TURNSTILE_CONFIG")
+      const ip = request.headers.get("CF-Connecting-IP");
+      let turnstileCfg = await getFromKV(
+        env.CROWDSECCFBOUNCERNS,
+        "TURNSTILE_CONFIG",
+      );
       if (turnstileCfg == null) {
-        console.log("No turnstile config found for zone")
-        return fetch(request)
+        console.log("No turnstile config found for zone");
+        return fetch(request);
       }
       if (typeof turnstileCfg === "string") {
-        console.log("Converting turnstile config to JSON")
-        turnstileCfg = JSON.parse(turnstileCfg)
-        writeToKV(env.CROWDSECCFBOUNCERNS, "TURNSTILE_CONFIG", turnstileCfg)
+        console.log("Converting turnstile config to JSON");
+        turnstileCfg = JSON.parse(turnstileCfg);
+        writeToKV(env.CROWDSECCFBOUNCERNS, "TURNSTILE_CONFIG", turnstileCfg);
       }
 
       if (!turnstileCfg[zoneForThisRequest]) {
-        console.log("No turnstile config found for zone")
-        return fetch(request)
+        console.log("No turnstile config found for zone");
+        return fetch(request);
       }
-      turnstileCfg = turnstileCfg[zoneForThisRequest]
+      turnstileCfg = turnstileCfg[zoneForThisRequest];
 
       const cookie = (0,dist/* parse */.qg)(request.headers.get("Cookie") || "");
       if (cookie[`${zoneForThisRequest}_captcha`] !== undefined) {
-        console.log("captchaAuth cookie is present")
+        console.log("captchaAuth cookie is present");
         // Check if the JWT token is valid
         try {
-          const decoded = await index_default.verify(cookie[`${zoneForThisRequest}_captcha`], turnstileCfg["secret"] + ip, {throwError: true});
-          return fetch(request)
+          const decoded = await index_default.verify(
+            cookie[`${zoneForThisRequest}_captcha`],
+            turnstileCfg["secret"] + ip,
+            { throwError: true },
+          );
+          return fetch(request);
         } catch (err) {
-          console.log(err)
+          console.log(err);
         }
-        console.log("jwt is invalid")
+        console.log("jwt is invalid");
       }
       if (request.method === "POST") {
         const formBody = await request.clone().formData();
-        if (formBody.get('cf-turnstile-response')) {
-          console.log("Handling turnstile post")
-          return await handleTurnstilePost(request, formBody, turnstileCfg["secret"], zoneForThisRequest)
+        if (formBody.get("cf-turnstile-response")) {
+          console.log("Handling turnstile post");
+          return await handleTurnstilePost(
+            request,
+            formBody,
+            turnstileCfg["secret"],
+            zoneForThisRequest,
+          );
         }
       }
 
@@ -7251,34 +7274,37 @@ const writeToKV = async (kv, key, value) => {
   </script>
   
   </html>
-      `
+      `;
       return new Response(captchaHTML, {
         headers: {
           "content-type": "text/html;charset=UTF-8",
         },
-        status: 200
+        status: 200,
       });
-    }
+    };
 
     const getRemediationForRequest = async (request, env) => {
-      console.log("Checking for decision against the IP")
+      console.log("Checking for decision against the IP");
       const clientIP = request.headers.get("CF-Connecting-IP");
       let value = await getFromKV(env.CROWDSECCFBOUNCERNS, clientIP);
       if (value !== null) {
-        return value
+        return value;
       }
 
-      console.log("Checking for decision against the IP ranges")
-      let actionByIPRange = await getFromKV(env.CROWDSECCFBOUNCERNS, "IP_RANGES");
+      console.log("Checking for decision against the IP ranges");
+      let actionByIPRange = await getFromKV(
+        env.CROWDSECCFBOUNCERNS,
+        "IP_RANGES",
+      );
       if (typeof actionByIPRange === "string") {
-        actionByIPRange = JSON.parse(actionByIPRange)
+        actionByIPRange = JSON.parse(actionByIPRange);
       }
       if (actionByIPRange !== null) {
         const clientIPAddr = ipaddr.parse(clientIP);
         for (const [range, action] of Object.entries(actionByIPRange)) {
           try {
             if (clientIPAddr.match(ipaddr.parseCIDR(range))) {
-              return action
+              return action;
             }
           } catch (error) {
             // This happens when trying to match IPv6 address with IPv4 CIDR (or vice versa)
@@ -7290,65 +7316,86 @@ const writeToKV = async (kv, key, value) => {
       const clientASN = request.cf.asn.toString();
       value = await getFromKV(env.CROWDSECCFBOUNCERNS, clientASN);
       if (value !== null) {
-        return value
+        return value;
       }
 
       // Check for decision against the country of the request
-      const clientCountry = request.cf.country.toLowerCase();
-      if (clientCountry !== null) {
-        value = await getFromKV(env.CROWDSECCFBOUNCERNS, clientCountry);
+      if (request.cf.country !== null) {
+        value = await getFromKV(
+          env.CROWDSECCFBOUNCERNS,
+          request.cf.country.toLowerCase(),
+        );
         if (value !== null) {
-          return value
+          return value;
         }
       }
-      return null
-    }
+      return null;
+    };
 
-    const incrementMetrics = async (metricName, ipType, origin, remediation_type) => {
+    const incrementMetrics = async (
+      metricName,
+      ipType,
+      origin,
+      remediation_type,
+    ) => {
       if (env.CROWDSECCFBOUNCERDB !== undefined) {
-        let parameters = [metricName, origin || "", remediation_type || "", ipType]
+        let parameters = [
+          metricName,
+          origin || "",
+          remediation_type || "",
+          ipType,
+        ];
         let query = `
           INSERT INTO metrics (val, metric_name, origin, remediation_type, ip_type)
           VALUES (1, ?, ?, ?, ?)
           ON CONFLICT(metric_name, origin, remediation_type, ip_type) DO UPDATE SET val=val+1
         `;
-
-        await env.CROWDSECCFBOUNCERDB
-          .prepare(query)
-          .bind(...parameters)
-          .run();
-
-      };
-    }
+        try {
+          await env.CROWDSECCFBOUNCERDB.prepare(query)
+            .bind(...parameters)
+            .run();
+        } catch (error) {
+          console.error("Error incrementing metrics:", error);
+        }
+      }
+    };
 
     const clientIP = request.headers.get("CF-Connecting-IP");
     const ipType = ipaddr.parse(clientIP).kind();
 
-    await incrementMetrics("processed", ipType)
+    await incrementMetrics("processed", ipType);
 
-
-    let remediation = await getRemediationForRequest(request, env)
+    let remediation = await getRemediationForRequest(request, env);
     if (remediation === null) {
-      console.log("No remediation found for request")
-      return fetch(request)
+      console.log("No remediation found for request");
+      return fetch(request);
     }
     if (typeof env.ACTIONS_BY_DOMAIN === "string") {
-      env.ACTIONS_BY_DOMAIN = JSON.parse(env.ACTIONS_BY_DOMAIN)
+      env.ACTIONS_BY_DOMAIN = JSON.parse(env.ACTIONS_BY_DOMAIN);
     }
-    const zoneForThisRequest = getZoneFromReqURL(request.url, env.ACTIONS_BY_DOMAIN);
-    console.log("Zone for this request is " + zoneForThisRequest)
-    remediation = getSupportedActionForZone(remediation, env.ACTIONS_BY_DOMAIN[zoneForThisRequest])
-    console.log("Remediation for request is " + remediation)
+    const zoneForThisRequest = getZoneFromReqURL(
+      request.url,
+      env.ACTIONS_BY_DOMAIN,
+    );
+    console.log("Zone for this request is " + zoneForThisRequest);
+    remediation = getSupportedActionForZone(
+      remediation,
+      env.ACTIONS_BY_DOMAIN[zoneForThisRequest],
+    );
+    console.log("Remediation for request is " + remediation);
     switch (remediation) {
       case "ban":
-        await incrementMetrics("dropped", ipType, "crowdsec", "ban")
-        return env.LOG_ONLY === "true" ? fetch(request) : await doBan()
+        await incrementMetrics("dropped", ipType, "crowdsec", "ban");
+        return env.LOG_ONLY === "true" ? fetch(request) : await doBan();
       case "captcha":
-        await incrementMetrics("dropped", ipType, "crowdsec", "captcha")
-        return env.LOG_ONLY === "true" ? fetch(request) : await doCaptcha(env, zoneForThisRequest)
+        await incrementMetrics("dropped", ipType, "crowdsec", "captcha");
+        return env.LOG_ONLY === "true"
+          ? fetch(request)
+          : await doCaptcha(env, zoneForThisRequest);
       default:
-        return fetch(request)
+        return fetch(request);
     }
-  }
+  },
 });
+
 export { worker as default };
